e63f02f99f5e2abfe718fa99832326bd4e607044
Siavash Aliyari <siavashaliyar79@gmail.com>
2023-04-22 08:39:44 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -16,5 +16,27 @@ const nextConfig = {
   },
 }
 
-module.exports = nextConfig
+
+const withPlugins = require('next-compose-plugins');
+const optimizedImages = require('next-optimized-images');
+module.exports = withPlugins([
+  [optimizedImages, {
+    mozjpeg: {
+      quality: 80,
+    },
+    pngquant: {
+      speed: 3,
+      strip: true,
+      verbose: true,
+    },
+    imagesPublicPath: '/peaked-coin/_next/static/images/',
+  }],
+  {
+    basePath: '/peaked-coin',
+    assetPrefix: '/peaked-coin/',
+    env,
+  },
+]);
+
+//module.exports = nextConfig
 // next.config.js
